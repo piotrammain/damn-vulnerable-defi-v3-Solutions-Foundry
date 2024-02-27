d406de84bec7d947ca94a7b8bcaadd45287595e9
piotrammain <piotrammain@mail.com>
2024-02-27 21:23:33 +0000

    Updating @openzeppelin-contracts-upgradeable remapping in AuthorizerUpgradeable.sol

--- src/wallet-mining/AuthorizerUpgradeable.sol
+++ src/wallet-mining/AuthorizerUpgradeable.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /**
  * @title AuthorizerUpgradeable
